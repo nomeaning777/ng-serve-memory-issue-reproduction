a5dfc436af23dc81c36dc6fa77a934f60befa1dc
Masafumi Yabu <m.yabu@coco-mero.com>
2020-07-29 17:43:22 +0900

    add encoding-japanese to dependency and use it from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as encoding from 'encoding-japanese';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-i18n';
+  sjisArray = encoding.convert(new Uint8Array([]), 'UTF8', 'SJIS');
 }
